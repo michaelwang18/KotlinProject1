abb5f9015b99336896ffd3a2231e9ee26c3ad07f
BT_1N3_33 <sample@bths.edu>
2024-10-10 09:34:26 -0400

    sad

--- app/src/main/java/com/example/project1_michael_wang/MainActivity.kt
+++ app/src/main/java/com/example/project1_michael_wang/MainActivity.kt
@@ -11,19 +11,69 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener  {
-    @SuppressLint("MissingInflatedId")
+    private lateinit var startButton: Button;
+    private lateinit var num1: TextView;
+    private lateinit var num2: TextView;
+    private var score = 0;
+    private var strike = 0;
+    private var started = false;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val startButton: Button = findViewById(R.id.startB)
-        val textView: TextView = findViewById(R.id.number1)
-        val duckView: TextView = findViewById(R.id.number2)
+        val num1: TextView = findViewById(R.id.number1)
+        val num2: TextView = findViewById(R.id.number2)
+        startAndRestart();
+        choosingNumber();
+
 
 
 
     }
+
+    private fun choosingNumber() {
+        num1.setOnClickListener{
+            if ( (num1.text) num2)
+
+        }
+        num2.setOnClickListener{
+
+
+        }
+
+
+    }
+
+
+    fun startAndRestart(){
+
+        startButton.setOnClickListener{
+
+            started = !started
+            startButton.text = if (started) "START" else "RESTART"
+
+        }
+
+    }
+
+    fun setValues(){
+        num1.text = 10.toString()
+        num2.text = 40.toString()
+
+    }
+
+
+
+
 
     override fun onClick(p0: View?) {
         TODO("Not yet implemented")
     }
+
+
+
+
+
 }
